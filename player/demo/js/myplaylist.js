2e1b7c939bef2ee2640da20894a16d9f2d7f137d
elizabethcase <elizabethhcase@gmail.com>
2014-06-01 18:11:40 -0700

    Changes worked on after deadline

--- player/demo/js/myplaylist.js
+++ player/demo/js/myplaylist.js
@@ -19,18 +19,18 @@ var myPlaylist = [
 
      {
         mp3:'player/demo/mix/firstsong.mp3',
-        title:'Fighting the Big 3',
-        artist:'StoryCorps',
+        title:'Pinballs Hold On Chicago',
+        artist:'Curious City',
         rating:4,
-        duration:'4:23',
+        duration:'13:34',
         cover:'story-corps.jpg'
     },
     {
         mp3:'player/demo/mix/firstsong.mp3',
-        title:'Fighting the Big 3',
-        artist:'StoryCorps',
+        title:'Connesson Pour Sortir Au Jour',
+        artist:'Chicago Symphony',
         rating:4,
-        duration:'4:23',
+        duration:'5:21',
         cover:'story-corps.jpg'
     },
     {
